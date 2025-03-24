550743f7c9ba57f60d76ea1689c6f1c424dd6a07
yeyu98 <763525908@qq.com>
2025-03-24 23:00:21 +0800

    feat: drizzle d1 local

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,3 +1,10 @@
+/*
+ * @Author: yeyu98
+ * @Date: 2025-03-24 22:08:34
+ * @LastEditors: yeyu98
+ * @LastEditTime: 2025-03-24 23:00:07
+ * @Description:
+ */
 import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
@@ -5,10 +12,11 @@ export default defineConfig({
 	out: './drizzle',
 	schema: './db/schema.ts',
 	dialect: 'sqlite',
-	driver: undefined,
+	// driver: 'd1-http',
 	dbCredentials: {
-		accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		token: process.env.CLOUDFLARE_TOKEN!,
-	}
+		url: './db/schema-v2.sql'
+		// accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
+		// databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
+		// token: process.env.CLOUDFLARE_TOKEN!,
+	},
 })
