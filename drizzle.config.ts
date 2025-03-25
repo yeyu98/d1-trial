f23d29d13ccb9b4679c8e3c23a7f419d5158bd6c
yeyu <763525908@qq.com>
2025-03-25 11:03:43 +0800

    feat: drizzle migration local database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,18 +5,10 @@
  * @LastEditTime: 2025-03-24 23:00:07
  * @Description:
  */
-import 'dotenv/config'
 import { defineConfig } from 'drizzle-kit'
 
 export default defineConfig({
 	out: './drizzle',
 	schema: './db/schema.ts',
 	dialect: 'sqlite',
-	// driver: 'd1-http',
-	dbCredentials: {
-		url: './db/schema-v2.sql'
-		// accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
-		// databaseId: process.env.CLOUDFLARE_DATABASE_ID!,
-		// token: process.env.CLOUDFLARE_TOKEN!,
-	},
 })
